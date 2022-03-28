9e3824837d4d2dbb17e678c3f18f1564ae027b9c
Wenchao Chen <chenwenchao@gmail.com>
2022-03-28 17:42:24 +1100

    Project Set up , added lint, packages and aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,42 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
+  env: {
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'import'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'no-unused-vars': 0,
+    'no-shadow': 0,
+    'no-console': 1,
+    'react/prop-types': 0,
+    '@typescript-eslint/no-unused-vars': [1, {ignoreRestSiblings: true}],
+    '@typescript-eslint/no-shadow': [2],
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-empty-function': [
+      'error',
+      {allow: ['arrowFunctions']},
+    ],
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          _screens: './src/screens',
+          _api: './src/api',
+          _types: './src/types',
+          _components: './src/components',
+          _styles: './src/styles',
+          _constants: './src/constants',
+          _utils: './src/utils',
+          '~': './src',
+        },
+      },
+    ],
+  ],
 };
