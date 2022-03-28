4725c8e52769bb85ec18d4fd11926c2f034c493c
Wenchao Chen <chenwenchao@gmail.com>
2022-03-28 18:12:22 +1100

    Android env setup

--- android/app/src/main/java/com/entainapp/MainApplication.java
+++ android/app/src/main/java/com/entainapp/MainApplication.java
@@ -1,7 +1,7 @@
 package com.entainapp;
-
 import android.app.Application;
 import android.content.Context;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
